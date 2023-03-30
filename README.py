b4ba651008aa0cd081c0ff7179f407d1ce447c5f
DjRezi <129382661+DjRezi@users.noreply.github.com>
2023-03-31 01:02:48 +0330

    Update README.py

--- README.py
+++ README.py
@@ -1,60 +1,52 @@
+import os
+import pickle
 import streamlit as st
 import pandas as pd
-import numpy as np
-import pickle
-import os
 
-# Get the absolute path of the directory containing this script
-dir_path = os.path.dirname(os.path.abspath(__file__))
-
-# Load the machine learning model
+# Load model
+dir_path = os.path.dirname(os.path.realpath(__file__))
 model_path = os.path.join(dir_path, 'model.pkl')
-if os.path.exists(model_path):
-    with open(model_path, 'rb') as file:
-        model = pickle.load(file)
+with open(model_path, 'rb') as file:
+    model = pickle.load(file)
+
+# Load sample dataset
+df = pd.read_csv('https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv')
+
+# Sidebar - User Input Features
+st.sidebar.title('User Input Features')
+# Collects user input features into dataframe
+def user_input_features():
+    age = st.sidebar.slider('Age', 1, 100, 25)
+    gender = st.sidebar.selectbox('Gender',('Male','Female'))
+    height = st.sidebar.slider('Height in cm', 100, 250, 175)
+    weight = st.sidebar.slider('Weight in kg', 20, 200, 70)
+    country = st.sidebar.selectbox('Country', ('USA', 'Canada', 'UK', 'Australia'))
+    blood_type = st.sidebar.selectbox('Blood Type', ('A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'))
+    data = {'age': age,
+            'gender': gender,
+            'height': height,
+            'weight': weight,
+            'country': country,
+            'blood_type': blood_type}
+    features = pd.DataFrame(data, index=[0])
+    return features
+
+df_new = user_input_features()
+
+# Main Panel
+st.header('Recommended Diet for Weight Management')
+
+# Show the user input features
+st.write('User Input features:')
+st.write(df_new)
+
+# Make recommendations
+prediction = model.predict(df_new)
+if prediction == 1:
+    st.write('We recommend a diet plan for weight gain.')
 else:
-    # Display error message if model file is not found
-    st.error("Model file not found. Please check that 'model.pkl' exists in the same directory as this script.")
-    st.stop()
+    st.write('We recommend a diet plan for weight loss.')
 
-# Load sample data for demo purposes
-data = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/diabetes.csv")
-
-# Create a form for the user to input their information
-st.title('Diet Recommendation App')
-st.header('Enter Your Information')
-height = st.slider('Height (in cm)', 100, 250, 150)
-weight = st.slider('Weight (in kg)', 30, 300, 70)
-age = st.slider('Age', 1, 120, 30)
-gender = st.radio('Gender', ('Male', 'Female'))
-blood_type = st.radio('Blood Type', ('A', 'B', 'AB', 'O'))
-country = st.text_input('Country')
-
-# Make a prediction using the machine learning model
-input_data = pd.DataFrame({
-    'Height': [height],
-    'Weight': [weight],
-    'Age': [age],
-    'Gender': [gender],
-    'Blood Type': [blood_type],
-    'Country': [country]
-})
-
-if 'Outcome' in data.columns:
-    X = data.drop('Outcome', axis=1)
-    y = data['Outcome']
-else:
-    X = data.drop('Class', axis=1)
-    y = data['Class']
-    
-model.fit(X, y)
-prediction = model.predict(input_data)
-
-# Display the recommended diet to the user
-st.header('Recommended Diet')
-st.write(prediction)
-
-# Add a chart to track progress over time
-st.header('Progress Chart')
-chart_data = pd.DataFrame(np.random.randn(20, 3), columns=['Week', 'Weight', 'BMI'])
-st.line_chart(chart_data)
+# Show sample dataset
+st.subheader('Sample Dataset')
+st.write(df.head())
