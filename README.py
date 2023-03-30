b080cf9fc0f2d38d6d907b9ffe6318f8229d9618
DjRezi <129382661+DjRezi@users.noreply.github.com>
2023-03-31 00:45:38 +0330

    Update README.py

--- README.py
+++ README.py
@@ -1,68 +1,38 @@
-
-# Import libraries
-import pandas as pd
-from sklearn.model_selection import train_test_split, cross_val_score
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import accuracy_score
-import joblib
 import streamlit as st
-from preprocessing import preprocess_data
+import pandas as pd
+import numpy as np
+import pickle
 
-# Load dataset
-df = pd.read_csv('diet_data.csv')
+# Load the machine learning model
+with open('model.pkl', 'rb') as file:
+    model = pickle.load(file)
 
-# Preprocess data
-df_preprocessed = preprocess_data(df)
+# Create a form for the user to input their information
+st.title('Diet Recommendation App')
+st.header('Enter Your Information')
+height = st.slider('Height (in cm)', 100, 250, 150)
+weight = st.slider('Weight (in kg)', 30, 300, 70)
+age = st.slider('Age', 1, 120, 30)
+gender = st.radio('Gender', ('Male', 'Female'))
+blood_type = st.radio('Blood Type', ('A', 'B', 'AB', 'O'))
+country = st.text_input('Country')
 
-# Sidebar for registration
-registration_status = st.sidebar.checkbox("Register as a new user")
-if registration_status:
-    # Function for registering new user
-    def register_user():
-        st.write("Please fill in the following information to register:")
-        name = st.text_input("Name:")
-        age = st.number_input("Age:", min_value=0, max_value=150)
-        gender = st.selectbox("Gender:", ['Male', 'Female'])
-        height = st.number_input("Height (cm):", min_value=0, max_value=250)
-        weight = st.number_input("Weight (kg):", min_value=0, max_value=500)
-        activity_level = st.selectbox("Activity level:", ['Sedentary', 'Lightly Active',
-                                                            'Moderately Active', 'Very Active'])
-        calories_per_day = st.number_input("Daily calories intake (kcal):", min_value=0, max_value=10000)
-        new_user_data = pd.DataFrame({
-            "name": [name],
-            "age": [age],
-            "gender": [gender],
-            "height": [height],
-            "weight": [weight],
-            "activity_level": [activity_level],
-            "calories_per_day": [calories_per_day]
-        })
-        return new_user_data
-        
-    # Get new user data and write to CSV file
-    new_user_data = register_user()
-    df_new_user = pd.concat([df_preprocessed, new_user_data])
-    try:
-        df_new_user.to_csv("diet_data.csv", index=False)
-    except:
-        st.error('Error: Could not register user.')
+# Make a prediction using the machine learning model
+input_data = pd.DataFrame({
+    'Height': [height],
+    'Weight': [weight],
+    'Age': [age],
+    'Gender': [gender],
+    'Blood Type': [blood_type],
+    'Country': [country]
+})
+prediction = model.predict(input_data)
 
-    # Update preprocessed data with new user data
-    df_preprocessed = df_new_user
+# Display the recommended diet to the user
+st.header('Recommended Diet')
+st.write(prediction)
 
-# Split into training and testing sets
-X = df_preprocessed.drop('BMI', axis=1)
-y = df_preprocessed['BMI']
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-# Train decision tree model using cross-validation
-dt_model = DecisionTreeClassifier(max_depth=3, random_state=42)
-cv_scores = cross_val_score(dt_model, X_train, y_train, cv=5)
-accuracy = cv_scores.mean()
-
-# Display model accuracy
-st.markdown(f"Model accuracy: {accuracy:.2f}")
-
-# Save model
-joblib.dump(dt_model, 'diet_planner_model.pkl')
-
+# Add a chart to track progress over time
+st.header('Progress Chart')
+chart_data = pd.DataFrame(np.random.randn(20, 3), columns=['Week', 'Weight', 'BMI'])
+st.line_chart(chart_data)
