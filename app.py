c1ec05c33b6abf25e0028d171f95da6c5dddf0ee
DjRezi <129382661+DjRezi@users.noreply.github.com>
2023-03-31 02:16:45 +0330

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ import requests
 import json
 import pandas as pd
 import streamlit as st
-!pip install scikit-learn
+pip install -U scikit-learn
+
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
