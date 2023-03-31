827b355bb2c62e70383ff595488742000cf562f2
DjRezi <129382661+DjRezi@users.noreply.github.com>
2023-03-31 21:16:52 +0330

    Update app.py

--- app.py
+++ app.py
@@ -2,97 +2,59 @@ import streamlit as st
 import pandas as pd
 import altair as alt
 
-# Define the datasets
-spending_categories = {
-    'Housing': 0.3,
-    'Transportation': 0.15,
-    'Food': 0.1,
-    'Utilities': 0.1,
-    'Insurance': 0.05,
-    'Medical': 0.05,
-    'Savings': 0.05,
-    'Entertainment': 0.05,
-    'Clothing': 0.03,
-    'Miscellaneous': 0.02
-}
+# Load data
+df_budget = pd.read_csv("https://raw.githubusercontent.com/djrezii/Streamlit-Finance-App/master/data/budget_data.csv")
+df_spending = pd.read_csv("https://raw.githubusercontent.com/djrezii/Streamlit-Finance-App/master/data/spending_data.csv")
+df_income = pd.read_csv("https://raw.githubusercontent.com/djrezii/Streamlit-Finance-App/master/data/income_data.csv")
 
-income_data = pd.DataFrame({
-    'Source': ['Salary', 'Investments', 'Freelance work'],
-    'Amount': [5000, 1000, 2000]
-})
+# Combine budget and spending data
+df_budget_spending = pd.merge(df_budget, df_spending, on="Category")
 
-expenses_data = pd.DataFrame({
-    'Category': ['Housing', 'Transportation', 'Food', 'Utilities', 'Insurance', 'Medical', 'Savings', 'Entertainment', 'Clothing', 'Miscellaneous'],
-    'Amount': [1500, 750, 500, 500, 250, 250, 250, 250, 150, 100]
-})
+# Calculate remaining budget
+df_budget_spending["Remaining"] = df_budget_spending["Budget"] - df_budget_spending["Spending"]
 
-# Define functions
-def generate_spending_chart():
-    chart_data = {key: val * monthly_income for key, val in spending_categories.items()}
-    chart_df = pd.DataFrame.from_dict(chart_data, orient="index", columns=["Amount"])
-    chart_df.reset_index(inplace=True)
-    chart_df.rename(columns={'index': 'Category'}, inplace=True)
+# Set page title
+st.set_page_config(page_title="Personal Finance Management")
 
-    chart = alt.Chart(chart_df).mark_bar().encode(
-        x='Amount:Q',
-        y=alt.Y('Category:N', sort=list(spending_categories.keys())),
-        color=alt.Color('Category:N', legend=None)
-    ).properties(
-        width=700,
-        height=400,
-        title='Monthly Spending'
-    )
+# Set app title
+st.title("Personal Finance Management")
 
-    return chart
+# Add subheader for budget
+st.subheader("Budget Overview")
 
-def generate_income_chart():
-    chart = alt.Chart(income_data).mark_bar().encode(
-        x='Amount:Q',
-        y='Source:N',
-        color=alt.Color('Source:N', legend=None)
-    ).properties(
-        width=700,
-        height=400,
-        title='Monthly Income'
-    )
+# Display budget table
+st.write(df_budget)
 
-    return chart
+# Create a chart of budget vs spending
+chart_data = df_budget_spending[["Category", "Budget", "Spending"]].set_index("Category").stack().reset_index()
+chart_data.columns = ["Category", "Budget/Spending", "Amount"]
+chart = alt.Chart(chart_data).mark_bar().encode(
+    x="Category",
+    y="Amount",
+    color="Budget/Spending"
+)
+st.altair_chart(chart, use_container_width=True)
 
-def generate_expenses_chart():
-    chart = alt.Chart(expenses_data).mark_bar().encode(
-        x='Amount:Q',
-        y='Category:N',
-        color=alt.Color('Category:N', legend=None)
-    ).properties(
-        width=700,
-        height=400,
-        title='Monthly Expenses'
-    )
+# Add subheader for income and spending
+st.subheader("Income and Spending Overview")
 
-    return chart
+# Display income and spending table
+st.write(df_income)
+st.write(df_spending)
 
-# Define the app
-def app():
-    st.title("Personal Finance Management")
+# Create a chart of income vs spending
+income_spending_data = pd.concat([df_income, df_spending])
+chart_data = income_spending_data[["Category", "Amount", "Type"]].set_index("Category").stack().reset_index()
+chart_data.columns = ["Category", "Type", "Amount"]
+chart = alt.Chart(chart_data).mark_bar().encode(
+    x="Category",
+    y="Amount",
+    color="Type"
+)
+st.altair_chart(chart, use_container_width=True)
 
-    monthly_income = st.number_input("What is your monthly income?", value=5000)
+# Add subheader for remaining budget
+st.subheader("Remaining Budget by Category")
 
-    st.subheader("Monthly Breakdown")
-
-    st.altair_chart(generate_spending_chart(), use_container_width=True)
-    st.altair_chart(generate_income_chart(), use_container_width=True)
-    st.altair_chart(generate_expenses_chart(), use_container_width=True)
-
-    st.subheader("Analysis")
-
-    expenses_total = expenses_data['Amount'].sum()
-    savings_total = monthly_income - expenses_total
-
-    st.write("Total Expenses: $", expenses_total)
-    st.write("Total Savings: $", savings_total)
-
-    st.subheader("Tips")
-
-    st.write("- Review your monthly spending and identify areas where you can cut back.")
-    st.write("- Consider setting up automatic savings transfers to help you reach your financial goals.")
-    st.write("- Check your credit score regularly and work to improve it if necessary.")
+# Display remaining budget table
+st.write(df_budget_spending[["Category", "Remaining"]])
