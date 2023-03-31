bfac9d9cee5748e5bc8e0e05585ff9f09d73f085
DjRezi <129382661+DjRezi@users.noreply.github.com>
2023-03-31 17:24:40 +0330

    Update app.py

--- app.py
+++ app.py
@@ -5,18 +5,19 @@ from datetime import datetime, timedelta
 import requests
 
 # Define layout
+st.set_page_config(page_title="Calorie Tracker", page_icon=":hamburger:", layout="wide")
 st.title("Calorie Tracker")
 
 # Define form fields
 with st.form(key='my_form'):
     food_name = st.text_input(label='Enter food name')
-    calories = st.number_input(label='Enter calories')
-    date = st.date_input(label='Enter date')
-    submit_button = st.form_submit_button(label='Submit')
+    calories = st.number_input(label='Enter calories', min_value=1)
+    date = st.date_input(label='Enter date', value=datetime.now())
+    submit_button = st.form_submit_button(label='Add')
 
 # Load data
 data = pd.DataFrame({
-    'food_name': ['chicken', 'pasta', 'salad'],
+    'food_name': ['Chicken', 'Pasta', 'Salad'],
     'calories': [450, 600, 200],
     'date': ['2022-03-29', '2022-03-30', '2022-03-31']
 })
@@ -32,19 +33,21 @@ week_data = data[data['date'] > last_week]
 total_calories = week_data['calories'].sum()
 
 # Display total calories for last week
-st.write("Total Calories Consumed Last Week:", total_calories)
+st.write(f"Total Calories Consumed Last Week: {total_calories}")
 
 # Calculate recommended daily caloric intake
-gender = st.radio(label="Select your gender", options=["Male", "Female"])
-age = st.number_input(label="Enter your age")
-height = st.number_input(label="Enter your height in cm")
-weight = st.number_input(label="Enter your weight in kg")
-activity_level = st.selectbox(label="Select your activity level", options=["Sedentary", "Lightly Active", "Moderately Active", "Very Active", "Extremely Active"])
-goal = st.radio(label="Select your goal", options=["Lose weight", "Maintain weight", "Gain weight"])
+st.write("")
+st.write("Recommended Daily Caloric Intake")
+gender = st.radio(label="Select your gender", options=["Male", "Female"], index=0)
+age = st.number_input(label="Enter your age", min_value=1, max_value=120, value=18)
+height = st.number_input(label="Enter your height in cm", min_value=50, max_value=300, value=170)
+weight = st.number_input(label="Enter your weight in kg", min_value=1, max_value=500, value=70)
+activity_level = st.selectbox(label="Select your activity level", options=["Sedentary", "Lightly Active", "Moderately Active", "Very Active", "Extremely Active"], index=2)
+goal = st.radio(label="Select your goal", options=["Lose weight", "Maintain weight", "Gain weight"], index=1)
 
 if submit_button:
     # Add new data to dataset
-    new_data = pd.DataFrame({'food_name': [food_name], 'calories': [calories], 'date': [date]})
+    new_data = pd.DataFrame({'food_name': [food_name.capitalize()], 'calories': [calories], 'date': [date]})
     data = pd.concat([data, new_data], ignore_index=True)
 
     # Calculate total calories for last week
@@ -53,7 +56,7 @@ if submit_button:
     total_calories = week_data['calories'].sum()
 
     # Display total calories for last week
-    st.write("Total Calories Consumed Last Week:", total_calories)
+    st.write(f"Total Calories Consumed Last Week: {total_calories}")
 
     # Calculate recommended daily caloric intake
     if gender == "Male":
@@ -65,17 +68,4 @@ if submit_button:
         tdee = bmr * 1.2
     elif activity_level == "Lightly Active":
         tdee = bmr * 1.375
-    elif activity_level == "Moderately Active":
-        tdee = bmr * 1.55
-    elif activity_level == "Very Active":
-        tdee = bmr * 1.725
-    else:
-        tdee = bmr * 1.9
-
-    if goal == "Lose weight":
-        daily_calories = tdee - 500
-    elif goal == "Gain weight":
-        daily_calories = tdee + 500
-    else:
-        daily_calories = tdee
-
+    elif activity_level
