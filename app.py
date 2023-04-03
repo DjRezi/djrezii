018d28feef05085ad8caa18cfd2b190ff588bae3
DjRezi <129382661+DjRezi@users.noreply.github.com>
2023-04-03 18:11:23 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,67 @@
-import streamlit as st
-import requests
-import base64
+# Required Libraries
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from keras.datasets import mnist
+from keras.layers import Dense, Flatten, Reshape, LeakyReLU
+from keras.models import Sequential
+from keras.optimizers import Adam
 
-def translate(text, target_language, api_key):
-    url = f'https://translation.googleapis.com/language/translate/v2?key={api_key}&q={text}&target={target_language}'
-    response = requests.get(url)
-    translated_text = response.json()['data']['translations'][0]['translatedText']
-    return translated_text
+# Function to build the generator model
+def build_generator(img_shape, zdim):
+    model = Sequential()
+    model.add(Dense(128, input_dim=zdim))
+    model.add(LeakyReLU(alpha=0.01))
+    model.add(Dense(28*28*1, activation='tanh'))
+    model.add(Reshape(img_shape))
+    return model
 
-def app():
-    st.set_page_config(page_title="Dinolingo", page_icon=":books:", layout="wide")
-    st.title('Dinolingo')
-    st.image('https://i.ibb.co/8s4HkLc/dinolingo-logo.png', use_column_width=True)
-    st.markdown('---')
-    text = st.text_input('Enter text to translate')
-    target_language = st.selectbox('Select target language', ['French', 'Spanish', 'German'])
-    api_key = 'your_api_key_here'
-    if st.button('Translate'):
-        translated_text = translate(text, target_language.lower(), api_key)
-        st.success(translated_text)
-    st.markdown('---')
-    st.subheader('About Dinolingo')
-    st.markdown('Dinolingo is a fun and interactive language learning platform for kids. It provides a wide range of language courses that are designed to be engaging and effective. With Dinolingo, kids can learn new languages through games, videos, songs, and stories. The platform offers courses in French, Spanish, German, and many other languages. Dinolingo is a great way for kids to learn new languages and explore new cultures.')
-    st.markdown('---')
-    st.subheader('Contact Us')
-    st.markdown('Email: info@dinolingo.com')
-    st.markdown('Phone: +1 (123) 456-7890')
-    st.markdown('Address: 123 Main Street, Anytown USA')
+# Function to build the discriminator model
+def build_discriminator(img_shape):
+    model = Sequential()
+    model.add(Flatten(input_shape=img_shape))
+    model.add(Dense(128))
+    model.add(LeakyReLU(alpha=0.01))
+    model.add(Dense(1, activation='sigmoid'))
+    return model
 
-def get_base64_of_app():
-    return base64.b64encode(app().to_bytes()).decode("utf-8")
+# Function to build the GAN model
+def build_gan(generator, discriminator):
+    discriminator.trainable = False
+    model = Sequential()
+    model.add(generator)
+    model.add(discriminator)
+    return model
 
-if __name__ == '__main__':
-    st.markdown(f'<iframe src="data:text/html;base64,{get_base64_of_app()}" width="700" height="600"></iframe>', unsafe_allow_html=True)
+# Function to train the GAN model
+def train_gan(generator, discriminator, gan, iterations, batch_size, interval):
+    # Load the dataset
+    (X_train, _), (_, _) = mnist.load_data()
+    # Rescale the images to the range [-1, 1]
+    X_train = X_train / 127.5 - 1.0
+    X_train = np.expand_dims(X_train, axis=3)
+    # Define the labels for real and fake images
+    real_labels = np.ones((batch_size, 1))
+    fake_labels = np.zeros((batch_size, 1))
+    # Lists to store losses and accuracies for plotting
+    d_losses, g_losses, accuracies, iteration_checks = [], [], [], []
+    # Train the model for the given number of iterations
+    for iteration in range(iterations):
+        # Select a random batch of real images from the dataset
+        ids = np.random.randint(0, X_train.shape[0], batch_size)
+        real_images = X_train[ids]
+        # Generate a batch of fake images from the generator
+        noise = np.random.normal(0, 1, (batch_size, 100))
+        fake_images = generator.predict(noise)
+        # Train the discriminator on the real and fake images
+        d_loss_real = discriminator.train_on_batch(real_images, real_labels)
+        d_loss_fake = discriminator.train_on_batch(fake_images, fake_labels)
+        d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
+        accuracy = 100 * np.mean(discriminator.predict(real_images) >= 0.5)
+        # Train the generator by fooling the discriminator
+        noise = np.random.normal(0, 1, (batch_size, 100))
+        g_loss = gan.train_on_batch(noise, real_labels)
+        # Append the losses and accuracies for plotting
+        if (iteration + 1) % interval == 0:
+           
